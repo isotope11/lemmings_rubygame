27df310ee7d7ee88eb9834fda9de8c30a069fb66
Josh Adams <josh@isotope11.com>
2013-02-19 16:56:38 -0600

    Add basic rubygame game

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ rubygame
 ## Installation
 Make sure you see [the rubygame project](https://github.com/rubygame/rubygame/wiki/Linux-Install-Guide) to see the rubygame installation docs
 
+## Learning about RubyGame
+https://github.com/rubygame/rubygame/blob/next/doc/getting_started.rdoc
+http://docs.rubygame.org/rdoc/current/
+http://rubygame.org/wiki/Guides_and_tutorials
+https://github.com/rubygame/rubygame/tree/next/samples
+
---
+++ lib/lemmings_rubygame.rb
@@ -0,0 +1,37 @@
+require 'rubygame'
+
+class LemmingsRubygame
+	def initialize
+		@screen = Rubygame::Screen.new [640,480], 0, [Rubygame::HWSURFACE, Rubygame::DOUBLEBUF]
+		@screen.title = "Lemmings"
+ 
+		@queue = Rubygame::EventQueue.new
+		@clock = Rubygame::Clock.new
+		@clock.target_framerate = 30
+	end
+ 
+	def run
+		loop do
+			update
+			draw
+			@clock.tick
+		end
+	end
+ 
+	def update
+		@queue.each do |ev|
+			case ev
+				when Rubygame::QuitEvent
+					Rubygame.quit
+					exit
+			end
+		end
+	end
+ 
+	def draw
+		@screen.flip
+	end
+end
+
+game = LemmingsRubygame.new
+game.run
